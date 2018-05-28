3d7a1356aaaf10fd571ea79bfcece09aea5046cd
A A <helpithinkihavehermes@gmail.com>
2018-05-28 01:54:43 -0400

    disabled form resizing

--- StardustSoundModder/Form1.Designer.cs
+++ StardustSoundModder/Form1.Designer.cs
@@ -446,9 +446,11 @@
             this.Controls.Add(this.startTimeLabel);
             this.Controls.Add(this.startTimeMinute);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Stardust Sound Patcher";
             this.Load += new System.EventHandler(this.Form1_Load);
