ca98f81d77173e8b243e7e37a232d6e0bc74beda
A A <helpithinkihavehermes@gmail.com>
2018-05-28 22:42:08 -0400

    update version

--- StardustSoundModder/AboutBox.Designer.cs
+++ StardustSoundModder/AboutBox.Designer.cs
@@ -82,7 +82,7 @@
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(253, 17);
             this.labelVersion.TabIndex = 0;
-            this.labelVersion.Text = "v1.0.0";
+            this.labelVersion.Text = "v1.0.1";
             this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // labelCopyright
